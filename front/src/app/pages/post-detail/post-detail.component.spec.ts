886770843a3fbe6e6d1b4397487a401f283d77aa
Athos Orío <athos.oc@gmail.com>
2021-12-11 16:18:22 +0100

    fix tests

--- front/src/app/pages/post-detail/post-detail.component.spec.ts
+++ front/src/app/pages/post-detail/post-detail.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostDetailComponent } from './post-detail.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('PostDetailComponent', () => {
   let component: PostDetailComponent;
   let fixture: ComponentFixture<PostDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ PostDetailComponent ]
     })
     .compileComponents();
